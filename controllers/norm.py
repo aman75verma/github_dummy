7d62525fe8e9d73924728bc41122e9ee26042543
Aman <ammuaman75@gmail.com>
2026-03-21 19:55:52 +0530

    dataframe done

--- controllers/norm.py
+++ controllers/norm.py
@@ -1,7 +1,12 @@
 import numpy as np 
+import pandas as pd
 
 X = np.array([[1,2,3] , [4,5,6]])
 u = np.mean(X,axis=0)
 dev = np.std(X , axis = 0) 
 print((X-u)/dev)
+
+df = pd.DataFrame(X)
+print(df)
+
 
